461fefed36c3633b629de470c8a1c501a1fb06eb
jzbass25 <jzbass25@yahoo.com>
2016-12-05 13:03:37 -0500

    Add files via upload

---
+++ DBCheckIn.java
@@ -0,0 +1,73 @@
+package hostelsolutions;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
+public class DBCheckIn {
+	private Connection conn = null;
+	private PreparedStatement statement = null;
+	private ResultSet rs = null;
+	private String sqlSelect;
+	private String sqlDelete;
+	private String sqlInsert;
+	
+	protected String uID;
+	protected String firstName;
+	protected String lastName;
+	protected String roomType;
+	protected String roomNum;
+	protected String checkI;
+	protected String checkO;
+	
+	// need timestamp of time cancelled
+	public DBCheckIn(String ID) {
+		uID = ID;
+	}
+	
+	public void checkIn(){
+		try {
+			conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/doggo","root","root");
+			conn.setAutoCommit(false); //transaction block start
+			
+			sqlSelect = "SELECT * FROM ARRIVING WHERE id_num = ?";
+			statement = (PreparedStatement) conn.prepareStatement(sqlSelect);
+			statement.setString(1, uID);
+			rs = statement.executeQuery();
+			while(rs.next()){
+				firstName = rs.getString("first_name");
+				lastName = rs.getString("last_name");
+				roomNum = rs.getString("room_type");
+				roomType = rs.getString("room_type");
+				checkI = rs.getString("check_in");
+				checkO = rs.getString("check_out");
+			}
+			
+			sqlInsert = "INSERT INTO in_house (id_num, last_name, first_name, room_num, room_type, check_in, check_out)"
+					+ " VALUES (?, ?, ?, ?, ?, ?, ?)";
+			statement = (PreparedStatement) conn.prepareStatement(sqlInsert);
+			statement.setString(1, uID);
+			statement.setString(2, lastName);
+			statement.setString(3, firstName);
+			statement.setString(4, roomNum);
+			statement.setString(5, roomType);
+			statement.setString(6, checkI);
+			statement.setString(7, checkO);
+			statement.execute();
+			
+			sqlDelete = "DELETE FROM ARRIVING WHERE id_num = ?";
+			statement = (PreparedStatement) conn.prepareStatement(sqlDelete);
+			statement.setString(1, uID);
+			statement.executeUpdate();
+			conn.commit(); //transaction block end
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
---
+++ PMSModifyReservation.java
@@ -0,0 +1,145 @@
+package hostelsolutions;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
+public class PMSModifyReservation extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel first = new JLabel("First Name: ");
+	private JLabel last = new JLabel("Last Name: ");
+	private JButton contact = new JButton("Contact Info");
+	private JTextField fname = new JTextField();
+	private JTextField lname = new JTextField();
+	
+	private JLabel cc = new JLabel("Credit Card #: ");
+	private JTextField card = new JTextField();
+	
+	private JLabel ci = new JLabel("Date In");
+	private JLabel co = new JLabel("Date Out");
+	private JFormattedTextField  dateIn;
+	private JFormattedTextField  dateOut;
+	private JButton roomNum = new JButton("Select Room");
+	
+	private JButton confirm = new JButton("Confirm");
+	private JButton cancel = new JButton("Cancel");
+	
+	private JPanel contactPanel = new JPanel();
+	private JPanel ctrlBtn = new JPanel();
+	
+	protected ContactInfo contactInfo = new ContactInfo();
+	private String uID;
+	DBModifyReservation db;
+	public PMSModifyReservation(String ID) {
+		super("Modify a Reservation");
+		
+		// gets the ID, sends it to the database to modify
+		uID = ID;
+		db = new DBModifyReservation(uID);
+		
+		setSize(500, 350);
+		this.setLocationRelativeTo(null);
+		
+		// formats date cells
+		Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String dateString = formatter.format(date);
+        dateIn = new JFormattedTextField(createFormatter("####-##-## ##:##:##"));
+        dateIn.setColumns(20);
+        dateIn.setText(dateString);
+		
+        dateOut = new JFormattedTextField(createFormatter("####-##-## ##:##:##"));
+        dateOut.setColumns(20);
+        dateOut.setText(dateString);
+        
+		contactPanel.setLayout(new GridLayout(6, 6));
+
+		contactPanel.add(first);
+		contactPanel.add(fname);
+		contactPanel.add(last);
+		contactPanel.add(lname);
+		contactPanel.add(ci);
+		contactPanel.add(dateIn);
+		contactPanel.add(co);
+		contactPanel.add(dateOut);
+		contactPanel.add(cc);
+		contactPanel.add(card);
+		contactPanel.add(roomNum);
+		
+		ctrlBtn.add(confirm);
+		ctrlBtn.add(cancel);
+		ctrlBtn.add(contact);
+		add(contactPanel, BorderLayout.NORTH);
+		add(ctrlBtn, BorderLayout.SOUTH);
+
+		contact.addActionListener(this);
+		cancel.addActionListener(this);
+		confirm.addActionListener(this);
+		roomNum.addActionListener(this);
+		setVisible(true);
+		
+		fname.setText(db.firstName);
+		lname.setText(db.lastName);
+		dateIn.setText(db.dateIn);
+		dateOut.setText(db.dateOut);
+		//rnum.setText(db.roomNum);
+		//rtype.setText(db.roomType);
+	}
+	
+    private MaskFormatter createFormatter(String s) {
+        MaskFormatter formatter = null;
+        try {
+            formatter = new MaskFormatter(s);
+        } catch (java.text.ParseException exc) {
+            System.err.println("formatter is incorrect, yyy-MM-dd hh:mm:ss: " + exc.getMessage());
+            System.exit(-1);
+        }
+        return formatter;
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == contact) {
+			PMSContactRes cont = new PMSContactRes(contactInfo);
+			cont.setVisible(true);
+			cont.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		}
+		
+		if (e.getSource() == cancel) {
+			this.setVisible(false);
+		}
+		
+		if (e.getSource() == confirm) {
+			// get all the data, send it to the constructor
+			String first = fname.getText();
+			String last = lname.getText();
+			String dateI = dateIn.getText();
+			String dateO = dateOut.getText();
+			String rNum = ""; // need to know how to get these
+			String rType ="";
+			
+			db.modData(first, last, dateI, dateO, rNum, rType);
+			this.setVisible(false);
+		}
+		
+		if (e.getSource() == roomNum) {
+			PMSRoomListing rooms = new PMSRoomListing();
+			rooms.setVisible(true);
+			rooms.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		}
+
+	}
+
+}
